cf005fae98c628a2e5614de9055bcf231c78b1ac
sonnnayyyf <s3978450@rmit.edu.vn>
2023-08-11 23:31:06 +0700

    bookwebsite-push8

--- COSC2430_Assignment1_s3978450_Tran_Hoang_Son/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbar = document.getElementsByClassName('navbar')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbar.classList.toggle('active')
-})
